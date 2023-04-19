933497e28357652c1d0c4d02567d074dba88ce5f
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-19 23:50:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -94,3 +94,17 @@ ReactDOM.render(
   </ul>,
   document.getElementById("root")
 );
+
+//math.js
+const pi = 3.1415962;
+
+function doublePi() {
+  return pi * 2;
+}
+
+function triplePi() {
+  return pi * 3;
+}
+
+export default pi;  
+export { doublePi, triplePi };
